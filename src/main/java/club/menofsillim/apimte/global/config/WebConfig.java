67e9ee54f82a598926eedfc5e1e91d738d35a3aa
truespring <truespring1@gmail.com>
2022-11-09 19:26:26 +0900

    도커 URL 추가

--- src/main/java/club/menofsillim/apimte/global/config/WebConfig.java
+++ src/main/java/club/menofsillim/apimte/global/config/WebConfig.java
@@ -7,12 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private static final String BASE_URL = "http://192.168.0.16:30040";
+    private static final String DOCKER_URL = "host.docker.internal:30040";
     private static final String KELLOGG_VPN_URL = "http://192.168.0.204:3000";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(BASE_URL, KELLOGG_VPN_URL)
+                .allowedOrigins(BASE_URL, KELLOGG_VPN_URL, DOCKER_URL)
                 .allowedMethods("*")
                 .maxAge(3000);
     }
